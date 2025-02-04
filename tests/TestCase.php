8b3fa59537519bb0b1ed38aacb8ce20feb7c9c95
ziming <ziming@users.noreply.github.com>
2025-02-05 02:46:13 +0800

    remove unused code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,11 +24,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
